f34e14bddf6ec3c5aae6de5e4eaf573181450271
mitraditya <am.19u10524@btech.nitdgp.ac.in>
2022-03-11 19:36:19 +0530

    Edited dashboard error.

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,9 +1,11 @@
 import * as React from "react";
 import { Card, CardContent, CardHeader } from '@material-ui/core';
 
-export default () => (
+const Dashboard = () => (
     <Card>
         <CardHeader title="Welcome to the admin panel!" />
         <CardContent>Cashback Duniya</CardContent>
     </Card>
 );
+
+export default Dashboard;
