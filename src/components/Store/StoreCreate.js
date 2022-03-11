c1704052af4c97a04618dd83a1f2f6f3fae20046
rishav4101 <rajkumarrishav4101@gmail.com>
2022-03-11 21:36:26 +0530

    [+] Add choices for networks

--- src/components/Store/StoreCreate.js
+++ src/components/Store/StoreCreate.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import {
   Create,
   SimpleForm,
@@ -13,54 +13,103 @@ import {
 
 const choices = [
   { id: "Cashback", name: "Cashback" },
-  { id: "Reward", name: "Reward" }
+  { id: "Reward", name: "Reward" },
 ];
 
 const StoreCreate = (props) => {
+  const [networks, setNetworks] = useState([]);
+  const [loaded, setLoaded] = useState(false);
+
+  const getNetworks = async () => {
+    try {
+      const { token } = JSON.parse(localStorage.getItem("auth"));
+      const res = await fetch(`${process.env.REACT_APP_BACKEND}/networks/`, {
+        headers: {
+          Authorization: token,
+        },
+      });
+      const networks = await res.json();
+      console.log(networks);
+      return networks;
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    console.log("USe Effect ran");
+    const load = async () => {
+      const networks = await getNetworks();
+      setNetworks(networks);
+      setLoaded(true);
+    };
+    load();
+  }, []);
   return (
-    <Create title="Create a Store" {...props}>
-      <SimpleForm>
-        <TextInput source="name" validate={required()} />
-        <TextInput multiline source="homepage" validate={required()} />
-        <TextInput multiline source="affiliate_link" validate={required()} />
-        <TextInput source="network_id" label="Network Id" validate={required()} />
-        <BooleanInput label="Featured" source="featured" />
-        <BooleanInput label="Cashback Enabled" source="cashback_enabled" />
-        <NumberInput source="cashback_percent" />
-        <NumberInput source="cashback" />
-        <AutocompleteInput
-          source="cashback_type"
-          choices={choices}
-          optionText="name" 
-          optionValue="id"
-        />
-        <TextInput source="amount_type" />
-        <TextInput source="rate_type" />
-        <TextInput source="cashback_was" />
-        <TextInput source="tracking_speed" />
-        <NumberInput source="visits" validate={required()} />
-        <TextInput source="terms" />
-        <TextInput source="tips" />
-        <TextInput source="h1" />
-        <TextInput source="h2" />
-        <TextInput source="description" />
-        <ImageInput source="image" accept="image/*">
-            <ImageField source="image" title="Image" />
-        </ImageInput>
-        <TextInput source="network_campaign_id" label="Network Campaign Id" />
-        <NumberInput source="coupon_count" />
-        <NumberInput source="clicks" />
-        <NumberInput source="payout_days" />
-        <BooleanInput source="is_claimable" />
-        <TextInput source="domain_name" />
-        <TextInput source="apply_coupon" />
-        <TextInput source="checkout_url" />
-        <TextInput source="slug" validate={required()} />
-        <TextInput source="status" validate={required()} />
-        <TextInput source="color1" />
-        <TextInput source="color2" />
-      </SimpleForm>
-    </Create>
+    <Fragment>
+      {loaded && (
+        <Create title="Create a Store" {...props}>
+          <SimpleForm>
+            <TextInput source="name" validate={required()} />
+            <TextInput multiline source="homepage" validate={required()} />
+            <TextInput
+              multiline
+              source="affiliate_link"
+              validate={required()}
+            />
+            {/* <TextInput
+          source="network_id"
+          label="Network Id"
+          validate={required()}
+        /> */}
+            <AutocompleteInput
+              source="network_id"
+              choices={networks}
+              optionText="name"
+              optionValue="id"
+            />
+            <BooleanInput label="Featured" source="featured" />
+            <BooleanInput label="Cashback Enabled" source="cashback_enabled" />
+            <NumberInput source="cashback_percent" />
+            <NumberInput source="cashback" />
+            <AutocompleteInput
+              source="cashback_type"
+              choices={choices}
+              optionText="name"
+              optionValue="id"
+            />
+            <TextInput source="amount_type" />
+            <TextInput source="rate_type" />
+            <TextInput source="cashback_was" />
+            <TextInput source="tracking_speed" />
+            <NumberInput source="visits" validate={required()} />
+            <TextInput source="terms" />
+            <TextInput source="tips" />
+            <TextInput source="h1" />
+            <TextInput source="h2" />
+            <TextInput source="description" />
+            <ImageInput source="image" accept="image/*">
+              <ImageField source="image" title="Image" />
+            </ImageInput>
+            <TextInput
+              source="network_campaign_id"
+              label="Network Campaign Id"
+            />
+            <NumberInput source="coupon_count" />
+            <NumberInput source="clicks" />
+            <NumberInput source="payout_days" />
+            <BooleanInput source="is_claimable" />
+            <TextInput source="domain_name" />
+            <TextInput source="apply_coupon" />
+            <TextInput source="checkout_url" />
+            <TextInput source="slug" validate={required()} />
+            <TextInput source="status" validate={required()} />
+            <TextInput source="color1" />
+            <TextInput source="color2" />
+          </SimpleForm>
+        </Create>
+      )}
+    </Fragment>
   );
 };
 
